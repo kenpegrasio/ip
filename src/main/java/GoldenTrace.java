36577ec08628515f593b56b4707d60e5580e6db7
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2026-02-04 06:35:01 +0800

    feat: new bot name, greeting, exit

---
+++ src/main/java/GoldenTrace.java
@@ -0,0 +1,17 @@
+public class GoldenTrace {
+    public static void main(String[] args) {
+        String logo =
+                  "  _____ _______\n"
+                + " / ____|__   __|\n"
+                + "| |  __   | |   \n"
+                + "| | |_ |  | |   \n"
+                + "| |__| |  | |   \n"
+                + " \\_____|  |_|   \n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
