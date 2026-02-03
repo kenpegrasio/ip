f35f47bd7e3598610a0b05773b484bb1df4dda68
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2026-02-04 06:47:27 +0800

    feat: echo

--- src/main/java/GoldenTrace.java
+++ src/main/java/GoldenTrace.java
@@ -1,17 +1,36 @@
+import java.util.Scanner;
+
 public class GoldenTrace {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /* Greeting */
         String logo =
-                  "  _____ _______\n"
-                + " / ____|__   __|\n"
-                + "| |  __   | |   \n"
-                + "| | |_ |  | |   \n"
-                + "| |__| |  | |   \n"
-                + " \\_____|  |_|   \n";
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+                  "       _____ _______\n"
+                + "      / ____|__   __|\n"
+                + "     | |  __   | |   \n"
+                + "     | | |_ |  | |   \n"
+                + "     | |__| |  | |   \n"
+                + "     \\_____|  |_|   \n";
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm\n" + logo);
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
+
+        /* Conversation block */
+        String line = scanner.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n");
+            System.out.println("    " + line);
+            System.out.println("    ____________________________________________________________\n");
+            line = scanner.nextLine();
+        }
+
+        /* Goodbye */
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+
+        scanner.close();
     }
 }
